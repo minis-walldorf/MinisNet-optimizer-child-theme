0cfc2c6b9583094ef2fb2db374380e8f1637fbe9
ujtmt-kilian-speder <kilian.speder@student.kit.edu>
2023-05-03 00:25:28 +0200

    add comments

--- frontpage/slider-static.php
+++ frontpage/slider-static.php
@@ -1,12 +1,15 @@
+<!--
+    Purpose:
+        Replace front page content header of Optimizer plugin to include a slider (pro feature of Optimzer)
+    Author: Kilian Speder
+    Date: 02.05.2023
+-->
+
 <?php global $optimizer; ?>
 
 <div id="stat_img" class="<?php if(!empty($optimizer['static_image_id']['url'])){ ?> stat_has_img<?php } ?><?php if(!empty($optimizer['static_gallery'])){ ?> stat_has_slideshow<?php } ?>">
-    
-
-	<?php $statimg = $optimizer['static_image_id']; ?>
     <?php
+        //import "FrontPage slider" image slider from Smart Slider 3 Plugin
         echo do_shortcode('[smartslider3 slider="2"]');
     ?>
-
-
 </div>
